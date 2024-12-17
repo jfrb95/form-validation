6d5456818b3ee50f4d1e8a9b95cfc9b2bb951cb2
Jako <43934830+jfrb95@users.noreply.github.com>
2024-12-17 06:56:35 +0000

    finish validation for each input

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const postalCodes = require("postal-codes-js");
 
 (function() {
 
+    //helper functions
     function kebabToCamel(str) {
         return str.replace(/-./g, x=>x[1].toUpperCase())
     };
@@ -21,47 +22,163 @@ const postalCodes = require("postal-codes-js");
     function hide(element) {
         element.style.display = 'none';
     }
+    function zipCodeMatchesCountryFormat(countryId, zipCode) {
 
-    const form = document.querySelector("form");
-    const submitButton = document.querySelector("button.submit");
-    submitButton.addEventListener("click", (event) => {
-        //event.preventDefault();
-    });
+        return typeof postalCodes.validate(countryId, zipCode) === 'string'
+            ? false
+            : true;
+    }
+    function passwordRequirements(password) {
+        let validationResults = [];
 
-    const formInputs = {};
-    for (const inputWrapper of document.querySelectorAll("form div.input-wrapper")) {
-        formInputs[kebabToCamel(inputWrapper.querySelector("input").id)] = {
-            label: inputWrapper.querySelector("label"),
-            input: inputWrapper.querySelector("input"),
-            feedback: inputWrapper.querySelector(".validation-feedback")
-        };
-    };
+        if (password.length < 8) {
+            validationResults.push('Password must be at least 8 characters.');
+        }
+        if (!/[0-9]/.test(password)) {
+            validationResults.push('Password must contain at least 1 number.');
+        }
+        if (!/[a-z]/.test(password)) {
+            validationResults.push('Password must contain at least 1 lower case character.');
+        }
+        if (!/[A-Z]/.test(password)) {
+            validationResults.push('Password must contain at least 1 upper case character.');
+        }
+        
+        return validationResults;
+    }        
+    function clearElement(element) {
+        element.replaceChildren();
+    }
 
-    formInputs.email.input.addEventListener("blur", (event) => {
+    //validation functions
+    function validateEmail() {
         const validity = formInputs.email.input.validity;
         const feedback = formInputs.email.feedback;
         let str = "";
 
-        if (validity.typeMismatch) {
-            str = "Please enter a valid email address.";
-            show(formInputs.email.feedback);
-        } else if (validity.valueMissing) {
+        if (validity.valueMissing) {
             str = "This is a required field.";
-            show(formInputs.email.feedback);
+            show(feedback);
+        } else if (validity.typeMismatch) {
+            str = "Please enter a valid email address.";
+            show(feedback);
         } else {
-            hide(formInputs.email.feedback);
+            hide(feedback);
         }
-        formInputs.email.feedback.textContent = str;
+        feedback.textContent = str;
+    }
+    function validateCountry() {
+        const validity = formInputs.country.input.validity;
+        const feedback = formInputs.email.feedback;
+        let str = '';
+
+        if (validity.valueMissing) {
+            str = "This is a required field.";
+            show(feedback);
+        } else {
+            hide(feedback);
+        }
+    }
+    function validateZipCode() {
+        const validity = formInputs.zipCode.input.validity;
+        const feedback = formInputs.zipCode.feedback;
+        let str = "";
+
+        const countryId = document.querySelector("#country").value;
+        const zipCodeValue = document.querySelector("#zip-code").value;
+
+        if (validity.valueMissing) {
+            str = "This is a required field.";
+            show(feedback);
+        } else if (!zipCodeMatchesCountryFormat(countryId, zipCodeValue)) {
+            str = "Please enter a valid zip code for your country.";
+            show(feedback);
+        } else {
+            hide(feedback);
+        }
+        feedback.textContent = str;
+    }
+    function validatePassword() {
+        const validity = formInputs.password.input.validity;
+        const feedback = formInputs.password.feedback;
+
+        const passwordValue = document.querySelector("#password").value;
+        let str = "";
+
+        if (validity.valueMissing) {
+            str = "This is a required field.";
+            clearElement(feedback);
+            feedback.textContent = str;
+            show(feedback);
+        } else if (passwordRequirements(passwordValue).length) {
+            clearElement(feedback);
+            for (const requirement of passwordRequirements(passwordValue)) {
+                const newP = document.createElement('p');
+                newP.textContent = requirement;
+                feedback.appendChild(newP);
+            }
+            show(feedback);
+        } else {
+            clearElement(feedback);
+            feedback.textContent = str;
+            hide(feedback);
+        }
+    }
+    function validateConfirmPassword() {
+        const validity = formInputs.confirmPassword.input.validity;
+        const feedback = formInputs.confirmPassword.feedback;
+
+        const passwordValue = document.querySelector("#password").value;
+        const confirmPasswordValue = document.querySelector("#confirm-password").value;
+        let str = "";
+
+        if (validity.valueMissing) {
+            str = 'This is a required field.';
+            show(feedback);
+        } else if (passwordValue !== confirmPasswordValue) {
+            str = 'Passwords must match.';
+            show(feedback);
+        } else {
+            hide(feedback);
+        }
+        feedback.textContent = str;
+    }
+
+    const form = document.querySelector("form");
+    const submitButton = document.querySelector("button.submit");
+    submitButton.addEventListener("click", (event) => {
+        //this function can be cleaned up
+        //ideally this would show which field(s) are invalid.
+        //  this would be difficult to implement at this stage
+        //  one way to avoid this would be to make each element
+        //  in javascript and deal with it there rather than dom.
+        //  Then, an input element would have a method to validate itself
+        //  so that it could be easy to call a specific validation
+        //  for a specific element elsewhere
+        event.preventDefault();
+        let formValid = true;
+        for (const inputWrapper of document.querySelectorAll("form div.input-wrapper")) {
+            if (!inputWrapper.querySelector("input, select").checkValidity()) {
+                log(kebabToCamel(inputWrapper.querySelector("input, select").id));
+                log("Form is invalid and has not been submitted. Please check the error messages.");
+                return;
+            }
+        }
+        log("Form is valid and has been submitted, thank you.");
     });
 
-    /*
-    formInputs.country.addEventListener("blur", (event) => {
-        let validity = "";
-        if (country.validity.tooShort) {
-            validity = "Please enter a valid email address.";
-        }
-        country.setCustomValidity(validity);
-        country.reportValidity();
-    });
-    */
+    const formInputs = {};
+    for (const inputWrapper of document.querySelectorAll("form div.input-wrapper")) {
+        formInputs[kebabToCamel(inputWrapper.querySelector("input, select").id)] = {
+            label: inputWrapper.querySelector("label"),
+            input: inputWrapper.querySelector("input, select"),
+            feedback: inputWrapper.querySelector(".validation-feedback")
+        };
+    };
+
+    formInputs.email.input.addEventListener("blur", validateEmail);
+    formInputs.country.input.addEventListener("blur", validateCountry);
+    formInputs.zipCode.input.addEventListener("blur", validateZipCode);
+    formInputs.password.input.addEventListener('blur', validatePassword);
+    formInputs.confirmPassword.input.addEventListener('blur', validateConfirmPassword);
 })();
